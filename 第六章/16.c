6337664684b86120077d1688bece737a20f965ed
YiShengping <yishengping@outlook.com>
2023-02-23 00:39:51 +0800

    第六章完工

---
+++ 第六章/16.c
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//int main() {
+//		int years = 0;
+//		float Deirdre = 100, Daphne = 100;
+//		for (; Deirdre<=Daphne; years++)
+//		{
+//			Deirdre += Deirdre * 0.05f;
+//			Daphne  += 10;
+//		}
+//		printf("Investment values after %d years:\n", years);
+//		printf("Daphne: $%.2f\n", Daphne);
+//		printf("Deirdre: $%.2f\n", Deirdre);
+//		printf("Deirdre(invest) > Daphne(invest)\n");
+//		return 0;
+//}
+//
+//
