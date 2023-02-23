779b63543c815e149ad1ded298390d3648c7e7c3
YiShengping <yishengping@outlook.com>
2023-02-23 18:11:41 +0800

    2/23

---
+++ 第六章/17.c
@@ -0,0 +1,10 @@
+//#include<stdio.h>
+//int main() {
+//	float win = 100, take = 10;
+//	int years = 0;
+//	for (; win >9; years++) {
+//		win += win * 0.08f,win -= 10;
+//		printf("After %d years remains $%g.\n", years, win);
+//	}
+//	printf("%d",years+1);
+//}
---
+++ 第六章/18.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//int main() {
+//	int friend = 5, i = 1;
+//	for (;friend<=150;)
+//	{
+//		printf("At %d weeks, Rabnud has", i);
+//		friend -= i++;
+//		friend *= 2;
+//		printf("%4d friends.\n", friend);
+//	}
+//	printf("At %d weeks, over Dunbar's number(150).\n", i);
+//}
