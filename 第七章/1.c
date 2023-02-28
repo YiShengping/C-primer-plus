71a89e2dca957b777c148e0ac869e8747058698e
YiShengping <yishengping@outlook.com>
2023-03-01 00:07:36 +0800

    test

--- 第七章/1.c
+++ 第七章/1.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-int main() {
-	char c;
-	while ((c=getchar()) != '#')
-	{
-		
-		printf("现在已输出");
-	}
-}
+//#include<stdio.h>
+//int main() {
+//	char c;
+//	while ((c=getchar()) != '#')
+//	{
+//		
+//		printf("现在已输出");
+//	}
+//}
