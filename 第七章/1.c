48c3bd34ef923b03ee9a6deb2ba9dd690424c57f
yishpi <yishpi@dingtalk.com>
2023-03-05 20:28:52 +0800

    机电2102晚自习

--- 第七章/1.c
+++ 第七章/1.c
@@ -1,9 +1,20 @@
-//#include<stdio.h>
-//int main() {
-//	char c;
-//	while ((c=getchar()) != '#')
-//	{
-//		
-//		printf("现在已输出");
-//	}
+//#include <stdio.h>
+//#define STOP '#'
+//
+//int main(void)
+//{
+//    int ch, space, enter, others;
+//    space = enter = others = 0;
+//
+//    printf("Please enter some characters (# to quit):\n");
+//    while ((ch = getchar()) != STOP)
+//    {
+//        (ch == ' ' ? ++space : (ch == '\n' ? ++enter : ++others));
+//    }
+//    printf("Here are the contents\n");
+//    printf("Space: %d\n", space);
+//    printf("Linefeed: %d\n", enter);
+//    printf("Others: %d\n", others);
+//
+//    return 0;
 //}
---
+++ 第七章/3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+	int figure=2;
+	while (figure !=0)
+	{
+		scanf_s("%d", &figure);
+	}
+}
