db6c6c483536e6baac8c56f8bb46466bd91ab0b9
yishengping <yishengping@outlook.com>
2023-05-10 14:53:45 +0800

    6

--- 第十一章/6.c
+++ 第十一章/6.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-int *is_within() {
+int is_within(const char *arrary, char c) {
 
+	for (int i = 0; *(arrary+i)!='\0'; i++)
+	{
+		if (*(arrary+i)=='c')
+		{
+			return 1;
+		}
+	}
+	return 0;
 }
 int main(void) {
 
