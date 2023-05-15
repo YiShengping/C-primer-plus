8cf90fa7d09835bccae918d8b3b41671f828e5d3
yishengping <yishengping@outlook.com>
2023-05-15 20:37:24 +0800

    晚自习

---
+++ 第十四章/4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define LEN 12
+struct MyStruct1
+{
+	char ins_num[LEN];
+	struct names
+	{
+
+	};
+
+};
+struct MyStruct2 {
+	char firstname[LEN];
+	char middlename[LEN];
+	char lastname[LEN];
+};
+int main(void) {
+	
+}
